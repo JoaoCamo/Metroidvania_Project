4f952a46e71218addb9dd73f1eb96153cc082883
João Camillo de Moura <joaocamo@gmail.com>
2023-04-06 16:51:13 -0300

    WIP_2 Extra

--- Assets/Scenes/Gameplay/Extra/Scripts/Projectile.cs
+++ Assets/Scenes/Gameplay/Extra/Scripts/Projectile.cs
@@ -2,17 +2,48 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Projectile : MonoBehaviour
+public class Projectile : Collidable
 {
-    // Start is called before the first frame update
-    void Start()
+    private int damage = 10;
+    private float pushForce = 7.5f;
+    public bool active;
+    public int type;
+    public Vector2 direction;
+    public float speed;
+
+    protected override void OnCollide(Collider2D coll)
     {
-        
+        if(coll.gameObject.CompareTag("Fighter") && coll.name == "Player")
+        {
+            Damage dmg = new Damage
+            {
+                damageAmount = damage,
+                origin = transform.position,
+                pushForce = pushForce
+            };
+            coll.SendMessage("ReceiveDamage", dmg);
+        }
+
+        if (!coll.gameObject.CompareTag("Bullet") && coll.gameObject.layer != 7)
+        {
+            Hide();
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    private void FixedUpdate()
     {
-        
+        transform.Translate(direction * speed * Time.deltaTime);
+    }
+
+    public void Show()
+    {
+        active = true;
+        gameObject.SetActive(active);
+    }
+
+    public void Hide()
+    {
+        active = false;
+        gameObject.SetActive(active);
     }
 }
