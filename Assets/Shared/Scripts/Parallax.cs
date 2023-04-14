2c48c68b5849da25321a1a6cfca64e7b7cee7e2b
João Camillo de Moura <joaocamo@gmail.com>
2023-04-14 17:03:04 -0300

    Update

--- Assets/Shared/Scripts/Parallax.cs
+++ Assets/Shared/Scripts/Parallax.cs
@@ -6,7 +6,14 @@ public class Parallax : MonoBehaviour
 {
     public float xSpeed;
     private Vector3 lastPos;
+    private Transform player;
+    private Vector3 velocity;
 
+    private void Start()
+    {
+        player = GameObject.Find("Player").transform;
+    }
+    
     private void FixedUpdate()
     {
         move();
@@ -17,10 +24,16 @@ public class Parallax : MonoBehaviour
         float xInput = Input.GetAxisRaw("Horizontal");
         if (Camera.main.transform.position.x != lastPos.x)
         {
-            Vector3 movement = new Vector3(xInput * xSpeed * Time.deltaTime, 0, 0);
-            if (movement.x > 0 || movement.x < 0)
+            if (player.GetComponent<Rigidbody2D>().velocity.x != 0)
             {
-                transform.Translate(movement);
+                velocity = new Vector3((player.GetComponent<Rigidbody2D>().velocity.x/150), 0, 0);
+                transform.Translate(velocity);
+            } else {
+                Vector3 movement = new Vector3(xInput * xSpeed * Time.deltaTime, 0, 0);
+                if (movement.x > 0 || movement.x < 0)
+                {
+                    transform.Translate(movement);
+                }
             }
         }
         lastPos = Camera.main.transform.position;
